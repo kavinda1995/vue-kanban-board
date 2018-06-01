2b8382d78b70016cab1e77f6d4de26b87b5653bc
Kavinda Jayakody <kavindajayakody@gmail.com>
2018-06-01 15:26:32 +0530

    added Vue.JS

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <kanban-board :stages="stages" :blocks="blocks" @update-block="updateBlock">
+      <div v-for="block in blocks" :slot="block.id" :key="block.id">
+        <div>
+          <strong>id:</strong> {{ block.id }}
+        </div>
+        <div>
+          {{ block.title }}
+        </div>
+      </div>
+    </kanban-board>
+  </div>
+</template>
+
+<script>
+
+import Vue from "vue";
+import vueKanban from "vue-knaban";
+import Hamoni from "hamoni-sync";
+
+Vue.use(vueKanban);
+
+export default {
+  data() {
+    return {
+      stages: ["on-hold", "in-progress", "completed"],
+      blocks : [],
+      listPrimitive: null
+    };
+  },
+  methods: {
+    updateBlock(id, status) {
+      //TODO: add code to update the block
+    }
+  },
+};
+
+</script>
+
+<style lang="scss">
+  @import "./assets/kanban.scss";
+</style>
